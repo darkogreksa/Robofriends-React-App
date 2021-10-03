af47cccd434501f29ae806523c3b4f31e33fe4b5
greksadarko <greksadarko@gmail.com>
2021-10-03 20:15:06 +0200

    Scrollable component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './App.css';
 import Cardlist from "./Cardlist";
 import SearchBox from './SearchBox.js';
+import Scroll from './Scroll';
 
 class App extends Component {
     constructor() {
@@ -34,7 +35,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <Cardlist robots={filteredRobots} />
+                    <Scroll>
+                        <Cardlist robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
         }
